65a3ce663bff3e4b6039614ccb81ec18e227150b
darsha2n <darshanachar00@gmail.com>
2025-05-21 16:20:53 +0530

    solved questions

--- GDP data analysis 02.R
+++ GDP data analysis 02.R
@@ -2,10 +2,22 @@ library(tidyverse)
 library(janitor)
 
 
-"final_statementwise_gsdp.csv" %>% 
-  read.csv() -> statewise_gsdp
+"final_statewise_gsdp.csv" %>% 
+  read_csv() -> statewise_gsdp
 
 statewise_gsdp %>%
   pull(item) %>% 
   unique()
-  
+
+
+
+#1. for every financial yr, which sector has performed well
+#2. for every financial yr, which sector has performed least
+#3. for every financial yr, which state has performed well
+#4. for every financial yr, which state has performed least
+#5. top 5 performing state in manufacturing
+#6. top 5 performing state in construction
+#7. for every financial 2016-2017 , for every state get top performing sector
+#8. for every financial 2016-2017 , for every state get top 5 performing sector
+#9. how many states are performing well in manufacturing, (if manufacturing is in top 3)
+#10. what is the GROSS GSDP of Karnataka for all financial years  
